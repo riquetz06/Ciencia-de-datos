569f20584c43d40c956fdd6d08041fd3b424b0d8
Riquet Zequeira Fernández <105960732+riquetz06@users.noreply.github.com>
2025-02-22 12:33:29 -0600

    Update Regresión lineal.R

--- Regresión lineal.R
+++ Regresión lineal.R
@@ -6,6 +6,7 @@ modelo<- lm(y~x1+x2, data=datos)
 ggplot(datos,aes(x=x,y=y))+geom_point()+geom_smooth(method=lm,se=FALSE)
 summary(modelo)
 
+# Modelo CAPM-Ejemplo
 datos <- read.csv(file.choose())
 datos
 library(ggplot2)
@@ -13,11 +14,12 @@ ggplot(datos,aes(x=Prima,y=Ln_AMXL))+geom_point()
 modelo<- lm(Ln_AMXL~Prima, data=datos)
 ggplot(datos,aes(x=Prima,y=Ln_AMXL))+geom_point()+geom_smooth(method=lm,se=FALSE)
 summary(modelo)
+
+# Homoscedasticidad y Autocorrelacion
 library(stargazer)
 stargazer(modelo,title="modelo estimado", type="text")
 library(lmtest)
 prueba_white<-bptest(modelo,~I(x1^2)+I(x2^2)+x1*x2, data=datos)
-
 print(prueba_white)
 # produce residual vs. fitted plot
 res <- resid(modelo)
@@ -26,6 +28,8 @@ plot(fitted(modelo), res)
 # add a horizontal line at 0 
 abline(0,0)
 dwtest(modelo,alternative="two.sided",iterations=1000)
+
+# Pruebas de normalidad
 library(fitdistrplus)
 ajuste_normal<-fitdist(data=modelo$residuals,distr="norm")
 plot(ajuste_normal)
